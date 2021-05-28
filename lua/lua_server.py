ad5587f7767d3a426f8b8013807dac7d744b42b0
lukas-ke <lukas-ke@users.noreply.github.com>
2021-05-28 20:13:59 +0200

    Use top-folder explicitly for log file with lua_server.py
    Use the project root folder for the server.log, to ensure it goes
    there regardless of working directory.
    
    Note: This should probably be controlled with command line flags.

--- lua/lua_server.py
+++ lua/lua_server.py
@@ -1,17 +1,22 @@
 from lsp.log import file_logger
+from lsp_server import lsp_io_server
 from lua import build_lua_doc
 from lua.builtins import add_built_ins
 from lua.lua_db import LuaDB
 from lua.lua_types import GlobalEnv
 from pathlib import Path
-from lsp_server import lsp_io_server
+
+
+def get_top_dir():
+    return Path(__file__).parent.parent.absolute()
 
 
 def get_workspace_dir():
     # Hardcoded path for require etc.
     # TODO: Determine in some other way (e.g. proper workspace
     # support)
-    workspace_dir = Path(__file__).parent.parent/"test/workspace"
+    top_dir = get_top_dir()
+    workspace_dir = top_dir / "test/workspace"
     assert workspace_dir.exists()
     return workspace_dir.absolute()
 
@@ -28,7 +33,8 @@ def create_db(log):
 
 
 def run():
-    with file_logger("server.log", 2) as log:
+    log_path = get_top_dir() / "server.log"
+    with file_logger(log_path, 2) as log:
         db = create_db(log)
         exit_code = lsp_io_server.run(db, log)
         exit(exit_code)
